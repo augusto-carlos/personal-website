0ecaff405196c6f1eab7b42993043b7d95249583
Augusto Carlos <62240490+augusto-carlos@users.noreply.github.com>
2020-09-26 18:47:57 +0100

    feat: add google-site-verification

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+    <meta name="google-site-verification" content="9ibkfnjvSE-WZKK4I8LQ_yGDVWze5bLuCoBh8U2iIDM" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes} className="dark">
